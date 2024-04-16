1fcd68116b14ebaed4243d3280fb2dd59af53880
OumaArera <johnouma999@gmail.com>
2024-04-16 12:08:02 +0300

    feat: initialize db

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,11 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_restful import Api, Resource, reqparse
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+import os
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 
+load_dotenv()
 
 app = Flask(__name__)
 
-# postgres = 
+postgres_db = os.environ.get("DATABASE_URL")
+secret_key = os.environ.get("SECRET_KEY")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = postgres_db
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = secret_key
+
+CORS(app, origins="*")
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+migrate = Migrate(app, db)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
