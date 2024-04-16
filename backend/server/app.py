a86e2adeb53f3bc360ef885659484122ecfb692c
OumaArera <johnouma999@gmail.com>
2024-04-16 15:45:32 +0300

    feat: fix relationships

--- backend/server/app.py
+++ backend/server/app.py
@@ -3,25 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 import os
-from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
-
-load_dotenv()
+from days.day import day_bp
 
 app = Flask(__name__)
 
-postgres_db = os.environ.get("DATABASE_URL")
-secret_key = os.environ.get("SECRET_KEY")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = postgres_db
+# Configure the app
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = secret_key
+app.config["JWT_SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+
+db = SQLAlchemy(app)  
+migrate = Migrate(app, db)  
+jwt = JWTManager(app) 
+
+
+app.register_blueprint(day_bp)
+
 
 CORS(app, origins="*")
-db = SQLAlchemy(app)
-jwt = JWTManager(app)
-migrate = Migrate(app, db)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
