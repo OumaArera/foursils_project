3e2a1c6144971d73ab87e04d2379dee58f126e50
neuman <owiroadams@gmail.com>
2024-04-18 13:52:27 +0300

    Neuman Correction

--- frontend/src/Tutor.jsx
+++ frontend/src/Tutor.jsx
@@ -1,67 +1,9 @@
-// import React, { useState } from "react";
-// import ModuleForm from "./Components/Module";
-// import LectureForm from "./Components/Lecture";
-// import CourseForm from "./Components/Course";
-// import NotesForm from "./Components/Notes";
-// import "./index.css";
-// import "./App.css"
-
-// function Tutor() {
-//   const [activeForm, setActiveForm] = useState(null);
-
-//   const handleButtonClick = (formName) => {
-//     setActiveForm(formName);
-//   };
-
-//   const renderForm = () => {
-//     switch (activeForm) {
-//       case "module":
-//         return <ModuleForm />;
-//       case "lecture":
-//         return <LectureForm />;
-//       case "course":
-//         return <CourseForm />;
-//       case "notes":
-//         return <NotesForm />;
-//       default:
-//         return null;
-//     }
-//   };
-
-//   return (
-//     <div className="tutor-container">
-//       <div>
-//         <h1>Welcome Tutor</h1>
-//       </div>
-//       <div className="buttons-top">
-//         <button className="button" onClick={() => handleButtonClick("module")} key="button1">
-//           Add Module
-//         </button>
-//         <button className="button" onClick={() => handleButtonClick("lecture")} key="button2">
-//           Add Lecture
-//         </button>
-//       </div>
-//       <div className="buttons-bottom">
-//         <button className="button" onClick={() => handleButtonClick("course")} key="button3">
-//           Add Course
-//         </button>
-//         <button className="button" onClick={() => handleButtonClick("notes")} key="button4">
-//           Add Notes
-//         </button>
-//       </div>
-//       {renderForm()}
-//     </div>
-//   );
-// }
-
-// export default Tutor;
-
 import React, { useState } from "react";
 import ModuleForm from "./Components/Module";
 import LectureForm from "./Components/Lecture";
 import CourseForm from "./Components/Course";
 import NotesForm from "./Components/Notes";
-import "./index.css";
+import "./index-Neuman.css";
 import "./App.css";
 
 function Tutor() {
